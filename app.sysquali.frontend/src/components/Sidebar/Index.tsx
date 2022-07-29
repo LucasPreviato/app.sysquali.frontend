f617054e26b734c06df7e2131ce939e42a3b0382
Lucas Previato <lucaspreviato@live.com>
2022-07-29 10:38:00 -0300

    correção tamanho do icone

--- app.sysquali.frontend/src/components/Sidebar/Index.tsx
+++ app.sysquali.frontend/src/components/Sidebar/Index.tsx
@@ -4,7 +4,7 @@ import { SideButtons } from "./SideButtons";
 import Rotina from "../assets/img/imgsidebar/Rotina.svg"
 
 export function Sidebar() {
-    
+
 
 
     return (
@@ -12,7 +12,7 @@ export function Sidebar() {
             <Popover className="bg-sysquali-900 w-20 h-screen hover:w-72 transition-all duration-500 absolute flex flex-col items-center overflow-y-scroll scrollbar-none ease-linear group pt-2">
                 <Popover.Button className="bg-brand-500 px-3  h-12 flex items-center rounded-full ">
                     <img title="String | undefined"
-                        className=" w-7 h-7"
+                        className=" w-6 h-6"
                         src="../src/components/assets/SysQuali.ico"
                     />
                     <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
@@ -20,9 +20,9 @@ export function Sidebar() {
                     </span>
                 </Popover.Button>
                 <Separator />
-                
+
                 <SideButtons />
-                
+
             </Popover>
         </>
     )
